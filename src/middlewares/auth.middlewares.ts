4032d12c9990c9de6914f0b7ec02706cfd3bae9c
Alexandre Marques (Xand2308) <xan372016@gmail.com>
2026-08-15 14:34:03 -0300

    feat: adiciona middleware de autenticação

---
+++ src/middlewares/auth.middlewares.ts
@@ -0,0 +1,12 @@
+import { FastifyRequest, FastifyReply } from "fastify";
+
+export const authenticate = async (
+  request: FastifyRequest,
+  reply: FastifyReply,
+): Promise<void> => {
+  try {
+    await request.jwtVerify();
+  } catch (err) {
+    reply.status(401).send({ message: "Token inválido ou expridado" });
+  }
+};
--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from "fastify";
 import { listProducts } from "../controllers/products.controller";
+import { authenticate } from "../middlewares/auth.middlewares";
+import { get } from "node:http";
 
 export default async function productRoutes(fastify: FastifyInstance) {
+  fastify.addHook("onRequest", authenticate);
+
   fastify.get("/", listProducts);
 }
-
-
